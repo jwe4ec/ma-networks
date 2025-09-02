8f60ed7aa89f0a1d8ca8ed77a67a2859f1672655
Jeremy Eberle <jwe4ec@virginia.edu>
2025-09-02 16:10:24 -0500

    Search for auxiliary variables WIP

---
+++ code/05_further_clean_other_data.R
@@ -0,0 +1,51 @@
+# ---------------------------------------------------------------------------- #
+# Further Clean Other Data
+# Author: Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# Before running script, restart R (CTRL+SHIFT+F10 on Windows) and set working 
+# directory to parent folder
+
+# ---------------------------------------------------------------------------- #
+# Store working directory, check correct R version, load packages ----
+# ---------------------------------------------------------------------------- #
+
+# Store working directory
+
+wd_dir <- getwd()
+
+# Load custom functions
+
+source("./code/01a_define_functions.R")
+
+# Check correct R version, load groundhog package, and specify groundhog_day
+
+groundhog_day <- version_control()
+
+# No packages loaded
+
+# ---------------------------------------------------------------------------- #
+# Import data ----
+# ---------------------------------------------------------------------------- #
+
+# Import data with final analysis sample
+
+load("./data/intermediate/net_dat_all_to_s6.RData")
+
+# Import restricted credibility data
+
+credibility_raw_rest2 <- read.csv("./data/intermediate/credibility_raw_rest2.csv")
+
+# ---------------------------------------------------------------------------- #
+# Further clean credibility data ----
+# ---------------------------------------------------------------------------- #
+
+# TODO: Restrict to final analysis sample
+
+
+
+
---
+++ code/06_search_for_aux_vars.R
@@ -0,0 +1,43 @@
+# ---------------------------------------------------------------------------- #
+# Search for Auxiliary Variables
+# Author: Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# Before running script, restart R (CTRL+SHIFT+F10 on Windows) and set working 
+# directory to parent folder
+
+# ---------------------------------------------------------------------------- #
+# Store working directory, check correct R version, load packages ----
+# ---------------------------------------------------------------------------- #
+
+# Store working directory
+
+wd_dir <- getwd()
+
+# Load custom functions
+
+source("./code/01a_define_functions.R")
+
+# Check correct R version, load groundhog package, and specify groundhog_day
+
+groundhog_day <- version_control()
+
+# No packages loaded
+
+# ---------------------------------------------------------------------------- #
+# Import data ----
+# ---------------------------------------------------------------------------- #
+
+# Import data with final analysis sample
+
+load("./data/intermediate/net_dat_all_to_s6.RData")
+
+# TODO: Import further clean demographics and credibility data
+
+
+
+
