6ecfdeb391282a712ad98ec9cea2af074cd1e594
Jeremy Eberle <jwe4ec@virginia.edu>
2025-11-01 17:50:59 -0500

    Add comments

--- code/06_further_clean_other_data.R
+++ code/06_further_clean_other_data.R
@@ -42,7 +42,13 @@ cred_dat <- cln_dat$credibility
 
 stopifnot(nrow(cred_dat) == 803)
 
+# Note: In centralized data cleaning ("TeachmanLab/MT-Data-ManagingAnxietyStudy-Cleaning" 
+# repo), values were recoded to 1:5, the displayed options (in contrast to present prereg)
+
 # TODO (prepare data to search for auxiliary variables)
+# - Compute mean of two confidence items in "further_clean_data.R"
+# - Then remove this script
+# - Consider moving further demographics cleaning to "further_clean_data.R"
 
 
 
