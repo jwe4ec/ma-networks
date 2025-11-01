3974091806fcd52839081ee72bb96bf571aa243f
Jeremy Eberle <jwe4ec@virginia.edu>
2025-11-01 16:04:59 -0500

    Compute scale-level missingness with new data

--- code/09_compute_scale_missingness.R
+++ code/09_compute_scale_missingness.R
@@ -16,7 +16,7 @@
 
 # Load custom functions
 
-source("./code/01a_define_functions.R")
+source(file.path("code", "01a_define_functions.R"))
 
 # Check correct R version, load groundhog package, and specify groundhog_day
 
@@ -28,10 +28,14 @@ groundhog_day <- version_control()
 # Import data ----
 # ---------------------------------------------------------------------------- #
 
-net_dat_all_to_s6_wide <- read.csv(file = "./data/intermediate/net_dat_all_to_s6_wide.csv")
+# Overall network dataset
 
-load("./results/descriptives/res_node_vars_itt_by_cond.RData")
-load("./results/descriptives/res_node_vars_compl_by_cond.RData")
+net_dat_all_to_s6 <- readRDS(file.path("data", "processed", "net_dat_all_to_s6.rds"))
+
+# Descriptives table for ITT participants
+
+desc_tbl_by_cond_itt_any_net <- readRDS(file.path("results", "descriptives", 
+                                                  "desc_tbl_by_cond_itt_any_net.rds"))
 
 # ---------------------------------------------------------------------------- #
 # Compute rates of scale-level missingness ----
@@ -39,18 +43,15 @@ load("./results/descriptives/res_node_vars_compl_by_cond.RData")
 
 # Compute number of ITT participants
 
-(N <- length(net_dat_all_to_s6_wide$participant_id)) == 729
+N <- length(unique(net_dat_all_to_s6$participant_id))
+stopifnot(N == 807)
 
-# TODO (create "all_item_missingness.txt", likely in "prepare_net_data.R"; see 
-# https://github.com/jwe4ec/fy7e6/blob/develop/code/06_further_clean_other_data_and_compute_item_missingness.R
-# from main outcomes paper): Define function to compute rate of scale-level missingness for given scale
-# based on number of ITT participants (N) and number of time points (J). Note:
-# Scale-level missingness can occur due to (a) attrition or (b) endorsing "prefer 
-# not to answer" for all items (see "all_item_missingness.txt" for rates of this).
-
-
-
-
+# Define function to compute rate of scale-level missingness for given scale 
+# based on number of ITT participants (N) and number of time points (J)
+# - Note: Scale-level missingness can occur when (a) the scale was not administered 
+#   (e.g., due to attrition or study error), (b) the data were lost (e.g., due to a 
+#   server issue), or (c) the participant selected “prefer not to answer” for all 
+#   items (see "all_item_missingness.txt" for rates)
 
 compute_scale_missingness_itt <- function(desc_tbl, scale, N, J) {
   n_cols <- c("n_POSITIVE", "n_FIFTY_FIFTY", "n_NEUTRAL")
@@ -62,26 +63,23 @@ compute_scale_missingness_itt <- function(desc_tbl, scale, N, J) {
   
   # Compute percentages of scale-level missingness
   
-  prop <- 1 - sum(dat, na.rm = TRUE)/(N*J)
-  percent <- prop*100
+  prop <- 1 - sum(dat, na.rm = TRUE) / (N * J)
+  perc <- prop * 100
   
-  cat(scale, ": ", percent, "%", "\n", sep = "")
+  cat(scale, ": ", perc, "%", "\n", sep = "")
 }
 
-# TODO (run for BBSIQ too): Run function and write results
+# Run function and write results
 
-
-
-
-
-missing_rates_path <- "./results/missing_rates/"
+missing_rates_path <- file.path("results", "missing_rates")
 dir.create(missing_rates_path)
 
-sink(file = paste0(missing_rates_path, "scale_missingness.txt"))
+sink(file.path(missing_rates_path, "scale_missingness.txt"))
 
 cat("Percentages of Scale-Level Missingness for Each Scale:", "\n\n")
 
-compute_scale_missingness_itt(res_node_vars_itt_by_cond, "rr_ns_mean",     N, 3)
-compute_scale_missingness_itt(res_node_vars_itt_by_cond, "rr_ps_mean_rev", N, 3)
+compute_scale_missingness_itt(desc_tbl_by_cond_itt_any_net, "rr_neg_thr_mean",     N, 3)
+compute_scale_missingness_itt(desc_tbl_by_cond_itt_any_net, "rr_pos_thr_mean_rev", N, 3)
+compute_scale_missingness_itt(desc_tbl_by_cond_itt_any_net, "bbsiq_neg_mean",      N, 3)
 
 sink()
